0bdde8bda05729cf4e106dbc1b54bd785786bec5
Jeff Taylor-Chang <jeff@jefftc.com>
2020-10-08 16:33:35 -0500

    Added jsdoc

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+/**
+ * Wrap an object with optional properties which should have default values if undefined.
+ *
+ * To use partials, set the element in the schema to `{}` to include all of its children that it may or may not have
+ *
+ * @param {*} optionalObj The object to wrap
+ * @param {*} schema The schema with default values which should be returned
+ * @param {*} allowPartials (Optional) True if elements in the wrapped object should be included even if they don't appear in the schema
+ */
 const oc = (optionalObj, schema, allowPartials = true) => {
   if (
     optionalObj === null ||
